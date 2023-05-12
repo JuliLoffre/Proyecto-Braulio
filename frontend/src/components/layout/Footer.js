5a03fbc67dadcc688ae892a125e4311c141ce4d7
Julian Loffreda <julianloffreda@gmail.com>
2023-05-12 23:59:04 +0200

    actualizado

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -3,7 +3,10 @@ import '../../styles/components/layout/Footer.css';
 const Footer = (props) => {
     return (
         <footer>
+            <br></br>
             <p>Diseñado por Julian Loffreda - 2023</p>
+            <br></br>
+
         </footer>
     );
 }
